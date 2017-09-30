4505053301d06ee395327d7d12d000f0b5d3938f
Jason Brooks <cs.jasonbrooks@gmail.com>
2017-09-30 09:48:05 -0400

    src and Room model

---
+++ src/Models/Room.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+import "User.go"
+
+type IRoom interface {
+  printPermissions()
+}
+
+type Room struct {
+  Users     []User
+  Name      string
+  Password  string
+}
+
+func (r Room) printRoom() {
+    fmt.Println("Name:", u.Name)
+}
+
+func main() {
+    u := User{ Permissions { Name: "Jason", Muted: true, ConnectedAccount: true, CanSkip: true }}
+    r := Room{ Users: [1]Users{u}, Name: "The Doom Room", Password: "you can't escape the doom"}
+    r.printPermissions()
+}
