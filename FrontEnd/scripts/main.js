4375fe762dd2bde392286a6e9301a68e1fe3a9d9
Jason Brooks <cs.jasonbrooks@gmail.com>
2017-09-30 14:18:05 -0400

    Moving to go

---
+++ FrontEnd/scripts/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('a').on('click', function(event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // hamburger menu
+  $('.header .menu').on('click', function() {
+    if($('.header').hasClass('open')) {
+      console.log('close');
+      $('.header').removeClass('open');
+    } else {
+      console.log('open');
+      $('.header').addClass('open');
+    }
+
+    return false;
+  });
+
+  // add class to dancing gifs
+  $('#login').click(function(){
+    $('#dance').addClass('active');
+  });
+});
+
